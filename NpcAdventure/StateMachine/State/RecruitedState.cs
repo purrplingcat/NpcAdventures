6b8107e79ce7d2c987add01f99a698b27e7b934b
Ellen Jana Fawkesová <ellenfawkes@gmail.com>
2019-11-09 12:31:23 +0100

    Closes #32 #34 added idle controller
    Base of idle controller finished
    
    Idle animation

--- NpcAdventure/StateMachine/State/RecruitedState.cs
+++ NpcAdventure/StateMachine/State/RecruitedState.cs
@@ -59,7 +59,7 @@ namespace NpcAdventure.StateMachine.State
                 this.StateMachine.Companion.setNewDialogue(dialogueText);
             this.CanCreateDialogue = true;
 
-            this.ai.Setup();
+            this.ai.Setup(this.StateMachine.ContentLoader);
         }
 
         /// <summary>
@@ -72,9 +72,9 @@ namespace NpcAdventure.StateMachine.State
 
             // And then play finish animation "end of route animation" when companion is recruited
             // Must be called via reflection, because they are private members of NPC class
-            NpcAdventureMod.Mod.Helper.Reflection.GetMethod(this.StateMachine.Companion, "finishEndOfRouteAnimation").Invoke();
+            NpcAdventureMod.Instance.Helper.Reflection.GetMethod(this.StateMachine.Companion, "finishEndOfRouteAnimation").Invoke();
             this.StateMachine.Companion.doingEndOfRouteAnimation.Value = false;
-            NpcAdventureMod.Mod.Helper.Reflection.GetField<Boolean>(this.StateMachine.Companion, "currentlyDoingEndOfRouteAnimation").SetValue(false);
+            NpcAdventureMod.Instance.Helper.Reflection.GetField<Boolean>(this.StateMachine.Companion, "currentlyDoingEndOfRouteAnimation").SetValue(false);
         }
 
         public override void Exit()
