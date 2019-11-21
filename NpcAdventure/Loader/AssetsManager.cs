ccc1da6a5273049c9518dc3ec143e8f3c960d8b1
Ellen Jana Fawkesová <ellenfawkes@gmail.com>
2019-11-21 14:28:51 +0100

    Remaked content loading and added localisation support

---
+++ NpcAdventure/Loader/AssetsManager.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Xna.Framework.Content;
+using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+
+namespace NpcAdventure.Loader
+{
+    /// <summary>
+    /// Mod's assets loader and editor.
+    /// </summary>
+    internal class AssetsManager : IAssetLoader, IAssetEditor
+    {
+        private readonly string modName;
+        private readonly string modAssetDir;
+        private readonly IMonitor monitor;
+
+        public AssetsManager(string modName, string modAssetDir, IContentHelper helper, IMonitor monitor)
+        {
+            if (string.IsNullOrEmpty(modAssetDir))
+            {
+                throw new ArgumentException("Mod assets directory must be set!", nameof(modAssetDir));
+            }
+
+            this.modName = modName;
+            this.modAssetDir = modAssetDir;
+            this.Helper = helper ?? throw new ArgumentNullException(nameof(helper));
+            this.monitor = monitor ?? throw new ArgumentNullException(nameof(monitor));
+        }
+
+        public IContentHelper Helper { get; }
+
+        /// <summary>
+        /// Can we load localisation for mod's asset?
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="asset"></param>
+        /// <returns></returns>
+        public bool CanEdit<T>(IAssetInfo asset)
+        {
+            // Can we load localisation only for localised assets
+            if (asset.AssetName.StartsWith(this.modName) && !string.IsNullOrEmpty(asset.Locale))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Is this asset mod's asset and we can load it?
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="asset"></param>
+        /// <returns></returns>
+        public bool CanLoad<T>(IAssetInfo asset)
+        {
+            if (!asset.AssetName.StartsWith(this.modName))
+                return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Patch Mod's asset contents with localised content
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="asset"></param>
+        public void Edit<T>(IAssetData asset)
+        {
+            string assetName = asset.AssetName.Replace(this.modName, this.modAssetDir);
+            string locale = asset.Locale;
+            string fileName = $"{assetName}.{locale}.json"; // Localised filename like Dialogue/Abigail.de-De.json for German localisation
+
+            try
+            {
+                this.monitor.VerboseLog($"Trying to load localised file {fileName} for {assetName}, locale {locale}");
+
+                var strings = asset.AsDictionary<string, string>().Data;
+                var localised = this.Helper.Load<Dictionary<string, string>>(fileName, ContentSource.ModFolder);
+
+                foreach(var pair in localised)
+                {
+                    strings[pair.Key] = pair.Value;
+                }
+            }
+            catch (ContentLoadException)
+            {
+                this.monitor.Log($"Loading of localised file {fileName}, fallback to non-localised file.", LogLevel.Alert);
+            }
+        }
+
+        /// <summary>
+        /// Load mod's asset contents
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="asset"></param>
+        /// <returns></returns>
+        public T Load<T>(IAssetInfo asset)
+        {
+            string assetName = asset.AssetName.Replace(this.modName, this.modAssetDir);
+            string filenName = $"{assetName}.json"; // Asset file name like `Dialogue/Abigail.json`
+
+            return this.Helper.Load<T>(filenName, ContentSource.ModFolder);
+        }
+    }
+}
--- NpcAdventure/NpcAdventureMod.cs
+++ NpcAdventure/NpcAdventureMod.cs
@@ -23,24 +23,32 @@ namespace NpcAdventure
         public override void Entry(IModHelper helper)
         {
             helper.Events.GameLoop.SaveLoaded += this.GameLoop_SaveLoaded;
+            helper.Events.Specialised.LoadStageChanged += this.Specialised_LoadStageChanged;
             helper.Events.GameLoop.ReturnedToTitle += this.GameLoop_ReturnedToTitle;
             helper.Events.GameLoop.DayEnding += this.GameLoop_DayEnding;
             helper.Events.GameLoop.DayStarted += this.GameLoop_DayStarted;
-            helper.Events.GameLoop.GameLaunched += this.GameLoop_GameLaunched;
 
             this.DialogueDriver = new DialogueDriver(helper.Events);
             this.HintDriver = new HintDriver(helper.Events);
             this.StuffDriver = new StuffDriver(helper.Events, helper.Data, this.Monitor);
-            this.contentLoader = new ContentLoader(helper.Content, helper.DirectoryPath, "assets", this.Monitor);
+            this.contentLoader = new ContentLoader(helper.Content, this.ModManifest.UniqueID, "assets", helper.DirectoryPath, this.Monitor);
             this.companionManager = new CompanionManager(this.DialogueDriver, this.HintDriver, this.Monitor);
         }
 
-        private void GameLoop_GameLaunched(object sender, GameLaunchedEventArgs e)
+        private void Specialised_LoadStageChanged(object sender, LoadStageChangedEventArgs e)
+        {
+            if (e.NewStage == StardewModdingAPI.Enums.LoadStage.Loaded)
+            {
+                this.PreloadAssets();
+            }
+        }
+
+        private void PreloadAssets()
         {
             /* Preload assets to cache */
             this.Monitor.Log("Preloading assets...", LogLevel.Info);
 
-            var dispositions = this.contentLoader.LoadStrings("CompanionDispositions");
+            var dispositions = this.contentLoader.LoadStrings("Data/CompanionDispositions");
 
             this.contentLoader.LoadStrings("Data/AnimationDescriptions");
             this.contentLoader.LoadStrings("Data/IdleBehaviors");
@@ -59,7 +67,6 @@ namespace NpcAdventure
 
         private void GameLoop_DayStarted(object sender, DayStartedEventArgs e)
         {
-            this.StuffDriver.ReviveDeliveredBags();
             this.companionManager.NewDaySetup();
         }
 
@@ -72,6 +79,7 @@ namespace NpcAdventure
         private void GameLoop_ReturnedToTitle(object sender, StardewModdingAPI.Events.ReturnedToTitleEventArgs e)
         {
             this.companionManager.UninitializeCompanions();
+            this.contentLoader.InvalidateCache();
         }
 
         private void GameLoop_SaveLoaded(object sender, StardewModdingAPI.Events.SaveLoadedEventArgs e)
