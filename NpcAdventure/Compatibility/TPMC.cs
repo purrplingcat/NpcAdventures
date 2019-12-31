cecebf35a6782d0a6b19284cfc2e2bb35fd8a2e5
Ellen Jana Fawkesová <ellenfawkes@gmail.com>
2019-12-31 02:08:02 +0100

    Added compatibility with Custom Kissing mod by Digus
    Also fixed bug in Abby recruited dialogue

---
+++ NpcAdventure/Compatibility/TPMC.cs
@@ -0,0 +1,32 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpcAdventure.Compatibility
+{
+    /// <summary>
+    /// Third party mod compatibility gateway
+    /// </summary>
+    internal class TPMC
+    {
+        public static TPMC Instance { get; private set; }
+
+        public CustomKissing CustomKissing { get; }
+
+        private TPMC(IModRegistry registry)
+        {
+            this.CustomKissing = new CustomKissing(registry);
+        }
+
+        public static void Setup(IModRegistry registry)
+        {
+            if (Instance == null)
+            {
+                Instance = new TPMC(registry);
+            }
+        }
+    }
+}
