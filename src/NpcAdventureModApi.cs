f5485dbf5ee2e5bb2ccbbd11114f8300d670ad8c
aedenthorn <aedenthorn1@gmail.com>
2020-08-10 11:32:16 -0400

    api fixes and additions

--- src/NpcAdventureModApi.cs
+++ src/NpcAdventureModApi.cs
@@ -1,72 +1,119 @@
 ﻿using NpcAdventure.StateMachine;
-using NpcAdventure.StateMachine.State;
 using StardewValley;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
+using static NpcAdventure.StateMachine.CompanionStateMachine;
 
 namespace NpcAdventure
 {
+    public interface INpcAdventureModApi
+    {
+        bool CanRecruitNPC();
+        IEnumerable<NPC> GetPossibleCompanions();
+        bool IsPossibleCompanion(string npc);
+        bool IsPossibleCompanion(NPC npc);
+        bool CanAskToFollow(NPC npc);
+        bool CanRecruit(Farmer farmer, NPC npc);
+        bool IsRecruited(NPC npc);
+        bool IsAvailable(NPC npc);
+        string GetNpcState(NPC npc);
+        bool RecruitCompanion(Farmer farmer, NPC npc);
+        string GetFriendSpecificDialogueText(Farmer farmer, NPC npc, string key);
+        string LoadString(string path);
+        string LoadString(string path, string substitution);
+        string LoadString(string path, string[] substitutions);
+    }
     public class NpcAdventureModApi
     {
         private NpcAdventureMod npcAdventureMod;
-
         internal NpcAdventureModApi(NpcAdventureMod npcAdventureMod)
         {
             this.npcAdventureMod = npcAdventureMod;
         }
-        public bool CanRecruit()
+        public bool CanRecruitNPC()
         {
-            return npcAdventureMod != null && npcAdventureMod.CompanionManager != null && npcAdventureMod.CompanionManager.CanRecruit();
+            return npcAdventureMod.CompanionManager.CanRecruit();
         }
-        public string[] GetPossibleCompanions()
+        public IEnumerable<NPC> GetPossibleCompanions()
         {
-            return npcAdventureMod.CompanionManager.PossibleCompanions.Keys.ToArray();
+            return npcAdventureMod.CompanionManager.PossibleCompanions.Select(s => s.Value.Companion);
         }
         public bool IsPossibleCompanion(string npc)
         {
-            return npcAdventureMod != null && npcAdventureMod.CompanionManager != null && npcAdventureMod.CompanionManager.PossibleCompanions != null && npcAdventureMod.CompanionManager.PossibleCompanions.ContainsKey(npc);
+            return npcAdventureMod.CompanionManager.PossibleCompanions.ContainsKey(npc);
         }
-        public bool CanRecruit(NPC npc)
+        public bool IsPossibleCompanion(NPC npc)
         {
-            return npcAdventureMod != null && npcAdventureMod.CompanionManager != null && npcAdventureMod.CompanionManager.PossibleCompanions != null && npcAdventureMod.CompanionManager.PossibleCompanions.TryGetValue(npc.Name, out CompanionStateMachine csm) && csm != null && npc != null && csm.Name == npc.Name && csm.CurrentStateFlag == CompanionStateMachine.StateFlag.AVAILABLE && csm.CanPerformAction();
+            return IsPossibleCompanion(npc.Name);
+        }
+        public bool CanAskToFollow(NPC npc)
+        {
+            if (!IsPossibleCompanion(npc))
+                return false;
+            var csm = npcAdventureMod.CompanionManager.PossibleCompanions[npc.Name];
+            return csm != null && csm.Name == npc.Name && csm.CurrentStateFlag == StateFlag.AVAILABLE && csm.CanPerformAction();
+        }
+        public bool CanRecruit(Farmer farmer, NPC npc)
+        {
+            if (!IsPossibleCompanion(npc))
+                return false;
+            var csm = npcAdventureMod.CompanionManager.PossibleCompanions[npc.Name];
+            return farmer.getFriendshipHeartLevelForNPC(npc.Name) >= csm.CompanionManager.Config.HeartThreshold && Game1.timeOfDay < 2200;
         }
         public bool IsRecruited(NPC npc)
         {
-            if (npcAdventureMod != null && npcAdventureMod.CompanionManager != null && npcAdventureMod.CompanionManager.PossibleCompanions != null && npcAdventureMod.CompanionManager.PossibleCompanions.TryGetValue(npc.Name, out CompanionStateMachine csm) && csm != null && npc != null && csm.Name == npc.Name && csm.CurrentStateFlag == CompanionStateMachine.StateFlag.RECRUITED)
-            {
-                return true;
-            }
-            return false;
+            if (!IsPossibleCompanion(npc))
+                return false;
+            var csm = npcAdventureMod.CompanionManager.PossibleCompanions[npc.Name];
+            return csm != null && csm.Name == npc.Name && csm.CurrentStateFlag == StateFlag.RECRUITED;
         }
         public bool IsAvailable(NPC npc)
         {
-            if (npcAdventureMod != null && npcAdventureMod.CompanionManager != null && npcAdventureMod.CompanionManager.PossibleCompanions != null && npcAdventureMod.CompanionManager.PossibleCompanions.TryGetValue(npc.Name, out CompanionStateMachine csm) && csm != null && npc != null && csm.Name == npc.Name && csm.CurrentStateFlag == CompanionStateMachine.StateFlag.AVAILABLE)
+            if (!IsPossibleCompanion(npc))
+                return false;
+            var csm = npcAdventureMod.CompanionManager.PossibleCompanions[npc.Name];
+            return csm != null && csm.Name == npc.Name && csm.CurrentStateFlag == StateFlag.AVAILABLE;
+        }
+        public string GetNpcState(NPC npc)
+        {
+            if (!IsPossibleCompanion(npc))
+                return null;
+            var csm = npcAdventureMod.CompanionManager.PossibleCompanions[npc.Name];
+            if (csm != null && csm.Name == npc.Name)
+                return Enum.GetName(typeof(StateFlag), csm.CurrentStateFlag);
+            return null;
+        }
+        public bool RecruitCompanion(Farmer farmer, NPC npc)
+        {
+            var csm = npcAdventureMod.CompanionManager.PossibleCompanions[npc.Name];
+
+            if (CanRecruitNPC() && CanAskToFollow(npc) && CanRecruit(farmer, npc))
             {
+                csm.Recruit();
                 return true;
             }
             return false;
         }
-        public int GetNpcState(NPC npc)
+        public string GetFriendSpecificDialogueText(Farmer farmer, NPC npc, string key)
         {
-            if (npcAdventureMod != null && npcAdventureMod.CompanionManager != null && npcAdventureMod.CompanionManager.PossibleCompanions != null && npcAdventureMod.CompanionManager.PossibleCompanions.TryGetValue(npc.Name, out CompanionStateMachine csm) && csm != null && npc != null && csm.Name == npc.Name)
-            {
-                return (int) csm.CurrentStateFlag;
-            }
-            return -1;
+            string text = null;
+            var csm = npcAdventureMod.CompanionManager.PossibleCompanions[npc.Name];
+            if (csm != null)
+                text = csm.Dialogues.GetFriendSpecificDialogueText(farmer, key);
+            return text;
         }
-        public bool RecruitCompanion(Farmer who, NPC npc, GameLocation location, bool skipDialogue = false)
+        public string LoadString(string path)
         {
-            if (!CanRecruit())
-                return false;
-            if (skipDialogue)
-            {
-                if(npcAdventureMod != null && npcAdventureMod.CompanionManager != null && npcAdventureMod.CompanionManager.PossibleCompanions != null && npcAdventureMod.CompanionManager.PossibleCompanions.TryGetValue(npc.Name, out CompanionStateMachine csm) && csm != null && npc != null && csm.Name == npc.Name && csm.CurrentStateFlag == CompanionStateMachine.StateFlag.AVAILABLE && csm.CanPerformAction() && Game1.player.getFriendshipHeartLevelForNPC(npc.Name) >= csm.CompanionManager.Config.HeartThreshold && Game1.timeOfDay < 2200)
-                        return true;
-                return false;
-            }
-            else
-                return npcAdventureMod != null && npcAdventureMod.CompanionManager != null && npcAdventureMod.CompanionManager.PossibleCompanions != null && npcAdventureMod.CompanionManager.PossibleCompanions.TryGetValue(npc.Name, out CompanionStateMachine csm) && csm != null && npc != null && csm.Name == npc.Name && csm.CheckAction(who, location);
+            return npcAdventureMod.ContentLoader.LoadString(path);
+        }
+        public string LoadString(string path, string substitution)
+        {
+            return npcAdventureMod.ContentLoader.LoadString(path, substitution);
+        }
+        public string LoadString(string path, string[] substitutions)
+        {
+            return npcAdventureMod.ContentLoader.LoadString(path, substitutions);
         }
     }
 }
